f3235528c100a0d5c9945efd0a7153ecf2e7172b
Oscar Mulder <32016388+OscarMulder@users.noreply.github.com>
2019-04-23 15:50:56 +0200

    Fix test main (#10)
    * Changed == FAILURE to != SUCCESS

--- test/test_main.c
+++ test/test_main.c
@@ -6,7 +6,7 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/18 16:37:32 by omulder        #+#    #+#                */
-/*   Updated: 2019/04/23 15:00:05 by omulder       ########   odam.nl         */
+/*   Updated: 2019/04/23 15:18:23 by omulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 
 int		main(void)
 {
-	if (test_prompt() == FUNCT_FAILURE)
+	if (test_prompt() != FUNCT_SUCCESS)
 		return (EXIT_FAILURE);
-	if (test_check_for_single_equalsign() == FUNCT_FAILURE)
+	if (test_check_for_single_equalsign() != FUNCT_SUCCESS)
 		return (EXIT_FAILURE);
-	if (test_free_and_return_null() == FUNCT_FAILURE)
+	if (test_free_and_return_null() != FUNCT_SUCCESS)
 		return (EXIT_FAILURE);
-	if (test_get_environ_cpy() == FUNCT_FAILURE)
+	if (test_get_environ_cpy() != FUNCT_SUCCESS)
 		return (EXIT_FAILURE);
-	if (test_param_to_env() == FUNCT_FAILURE)
+	if (test_param_to_env() != FUNCT_SUCCESS)
 		return (EXIT_FAILURE);
-	if (test_term_is_valid() == FUNCT_FAILURE)
+	if (test_term_is_valid() != FUNCT_SUCCESS)
 		return (EXIT_FAILURE);
-	if (test_term_init_struct() == FUNCT_FAILURE)
+	if (test_term_init_struct() != FUNCT_SUCCESS)
 		return (EXIT_FAILURE);
-	if (test_term_free_struct() == FUNCT_FAILURE)
+	if (test_term_free_struct() != FUNCT_SUCCESS)
 		return (EXIT_FAILURE);
-	if (test_term_get_attributes() == FUNCT_FAILURE)
+	if (test_term_get_attributes() != FUNCT_SUCCESS)
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
