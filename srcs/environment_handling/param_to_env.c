b5c815852229252a4c94c8613e4cc1ecace48b34
marijnvanh <47252289+marijnvanh@users.noreply.github.com>
2019-04-25 13:47:14 +0200

    Added malloc protection to param_to_env (#30)
    * Added malloc protection to param_to_env
    
    * fixed malloc protection in param_to_env update
    
    * Removed malloc check function and replaced with return NULL
    
    *  index = 0 now inside while loop
    
    * index = i

--- srcs/environment_handling/param_to_env.c
+++ srcs/environment_handling/param_to_env.c
@@ -6,33 +6,38 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/03 18:45:30 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/04/19 20:31:36 by jbrinksm      ########   odam.nl         */
+/*   Updated: 2019/04/25 13:03:10 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
 
-char	*param_to_env(char *parameter, char **vshenviron)
+/*
+** add errno here
+*/
+
+char		*param_to_env(char *parameter, char **vshenviron)
 {
 	int		env_index;
-	int		index;
+	int		i;
 	char	*name;
 	char	*value;
 
 	env_index = 0;
-	index = 0;
 	while (vshenviron[env_index] != NULL)
 	{
+		i = 0;
 		name = ft_strcdup(vshenviron[env_index], '=');
+		if (name == NULL)
+			return (NULL);
 		if (ft_strcmp(parameter, name) == 0)
 		{
-			while (vshenviron[env_index][index] != '=')
-				index++;
-			value = ft_strdup(&(vshenviron[env_index][index + 1]));
+			while (vshenviron[env_index][i] != '=')
+				i++;
+			value = ft_strdup(&(vshenviron[env_index][i + 1]));
 			ft_strdel(&name);
 			return (value);
 		}
-		index = 0;
 		ft_strdel(&name);
 		env_index++;
 	}
