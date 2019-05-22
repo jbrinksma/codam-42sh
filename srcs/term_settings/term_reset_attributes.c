f028762ff102460632c67ddad323610593115986
Jorn Brinksma <44733663+jbrinksma@users.noreply.github.com>
2019-05-22 11:49:22 +0200

    term_reset now returns error on error (#70)
    * fixed issue
    
    * added comment for future revision

--- srcs/term_settings/term_reset_attributes.c
+++ srcs/term_settings/term_reset_attributes.c
@@ -6,11 +6,15 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/18 18:19:47 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/04/18 18:19:56 by jbrinksm      ########   odam.nl         */
+/*   Updated: 2019/05/21 20:41:40 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
+
+/*
+**	We will need to implement errno return values for this function.
+*/
 
 int		term_reset(t_term *term_p)
 {
@@ -19,6 +23,6 @@ int		term_reset(t_term *term_p)
 	/* Is TCSANOW flag correct? */
 	ret = tcsetattr(STDIN_FILENO, TCSANOW, term_p->old_termios_p);
 	if (ret == -1)
-		return (FUNCT_SUCCESS);
+		return (FUNCT_ERROR);
 	return (FUNCT_SUCCESS);
 }
