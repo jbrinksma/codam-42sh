3b8b2673ea158ce315792f43696c1d9e0afa63f4
marijnvanh <47252289+marijnvanh@users.noreply.github.com>
2019-07-30 15:30:57 +0200

    Leak fix for alias replace (#169)

--- srcs/alias/alias_replace.c
+++ srcs/alias/alias_replace.c
@@ -6,7 +6,7 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/07/26 20:29:50 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/07/28 17:17:12 by omulder       ########   odam.nl         */
+/*   Updated: 2019/07/30 14:27:28 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ static void	alias_combine_tokenlsts(t_tokenlst *probe, t_tokenlst *new_tokenlst)
 		start_end = start_end->next;
 	ft_memdel((void**)&start_end->next);
 	start_end->next = probe->next->next;
+	ft_strdel(&probe->next->value);
 	ft_memdel((void**)&probe->next);
 	probe->next = new_tokenlst;
 }
