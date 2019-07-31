e6d8eaed69b1d9a227ef5563892c40da06dae6b2
Oscar Mulder <32016388+OscarMulder@users.noreply.github.com>
2019-07-31 15:43:46 +0200

    Fixes cursor going over 'prompt' on pressing home (#174)
    * Fixes cursor going over 'prompt' on pressing home
    
    * Code cleanup
    
    * Add missing bracket
    
    * fix error in comment

--- srcs/input_handling/input_parse_home.c
+++ srcs/input_handling/input_parse_home.c
@@ -3,20 +3,27 @@
 /*                                                        ::::::::            */
 /*   input_parse_home.c                                 :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: rkuijper <rkuijper@student.codam.nl>         +#+                     */
+/*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/16 13:37:33 by rkuijper       #+#    #+#                */
-/*   Updated: 2019/07/15 15:53:58 by omulder       ########   odam.nl         */
+/*   Updated: 2019/07/31 15:32:05 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
 
+/*
+** The nested if condition is to prevent the cursor to go over the 'prompt' part
+** (vsh >) of the terminal line, while also preventing other input functions
+** from executing the keypress.
+*/
+
 int	input_parse_home(t_inputdata *data)
 {
 	if ((data->input_state == INPUT_BRACE && data->c == 'H') || data->c == '\1')
 	{
-		ft_printf("\e[%dD", data->index);
+		if (data->index > 0)
+			ft_printf("\e[%dD", data->index);
 		data->index = 0;
 		data->input_state = INPUT_NONE;
 		return (FUNCT_SUCCESS);
