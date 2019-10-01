74b645d262b35143708f27f4488649ec5cc5b153
Thijs de Jong <tde-jong@student.codam.nl>
2019-10-01 14:25:35 +0200

    Update ft_is_regular_file.c (#307)

--- libft/ft_is_regular_file.c
+++ libft/ft_is_regular_file.c
@@ -6,7 +6,7 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/08/04 12:11:41 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/09/04 11:14:54 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/10/01 14:14:54 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		ft_is_regular_file(const char *path)
 {
 	struct stat stat_path;
 
-	if (lstat(path, &stat_path) == -1)
+	if (stat(path, &stat_path) == -1)
 		return (-1);
 	return (S_ISREG(stat_path.st_mode));
 }
